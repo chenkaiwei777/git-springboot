d4dc159b21599af50c840f63f7e0fa316c020f94
chenkaiwei777 <chenkaiwei327@163.com>
2019-10-15 15:59:34 +0800

    第一次commit

---
+++ git-springboot/src/main/java/com/yzit/GitSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.yzit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSpringbootApplication.class, args);
+    }
+
+}
